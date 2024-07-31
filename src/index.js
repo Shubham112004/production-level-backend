4e94e3b693bc7e50166cde9477f8c30a41ff2d8c
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-07-31 11:02:37 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv"
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+/*
+
+*****first approch********
+
+import express from "express"
+const app = express()
+
+    (async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            app.on("error", (error) => {
+                console.log("ERROR: ", error);
+                throw error
+            })
+
+            app.listen(process.env.PORT, () => {
+                console.log(`App is listining on port ${process.env.PORT}`);
+            })
+
+        } catch (error) {
+            console.log("ERROR: ", error);
+            throw error
+        }
+    })()
+
+*/
